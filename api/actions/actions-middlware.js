955f51355e3c93078d63b4d67fed12b8461e1016
unknown <joey.bertschler@gmail.com>
2021-12-13 10:59:03 +0400

    messy mvp

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,38 @@
-// add middlewares here related to actions
+// relocated
+
+// const Action = require("../actions/actions-model");
+
+// // ACTION MIDDLEWARE STARTS HERE
+// const checkActionId = async (req, res, next) => {
+//     const id = req.params.id
+//     const actionId = await Action.get(id)
+//     if(!actionId){
+//         res.status(404).json({
+//             message: "Action not found"
+//         })
+//     } else {
+//         req.actionId = actionId
+//         next()
+//     }
+// }
+
+// const checkAction = async (req, res, next) => {
+//     const { description, notes } = req.body
+//     if(!description){
+//         res.status(400).json({
+//             message: "Missing required description field"
+//         })
+//     } else if(!notes){
+//         res.status(400).json({
+//             message: "Missing required notes field"
+//         })
+//     } else {
+//         next()
+//     }
+// }
+// // ACTION MIDDLEWARE ENDS HERE
+
+// module.exports = {
+//     checkActionId,
+//     checkAction
+// }
